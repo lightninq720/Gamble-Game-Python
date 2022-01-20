beb8806febb4376b0bb47a54fb89320b62f6bfcc
lightninq <82113835+lightninq720@users.noreply.github.com>
2022-01-20 20:51:36 +0000

    Update Gamble Game.py

--- Gamble Game.py
+++ Gamble Game.py
@@ -1,22 +1,42 @@
-import random
-money = 500
-print('Welcome to the gambling game!')
-loop = True
-while loop == True:
+import random, time
+
+
+#vars
+coins = 500
+attempts = 0
+successful_attempts = 0
+unsuccessful_attempts = 0
+
+#game
+while True:
+    if coins == 0:
+        print("You have gone bankrupt!!")
+        print("\nGame will end in 3 seconds")
+        time.sleep(3)
+        break
+    print("Current Stats:")
+    print("Overall Attempts:", attempts)
+    print("Successful Attempts", successful_attempts)
+    print("Unsuccessful Attempts:", unsuccessful_attempts)
+    print(f"You have £{coins}")
     try:
-        print('You have $' + str(money))
-        bet = int(input('How much do you want to bet? '))
-        if bet > money:
-            print('Invalid Bet')
-        elif bet <= 0:
-            print('Invalid bet')
+        gamble = int(input("How much do you want to gamble?\n"))
+        if gamble > coins:
+            print("Invalid Bet")
+        elif gamble <= 0:
+            print("Invalid Bet")
         else:
             chance = random.randint(1,2)
+            attempts += 1
             if chance == 1:
-                print('You Won!')
-                money += bet
+                print("You Won!")
+                coins += gamble
+                successful_attempts +=1
             else:
-                print('You Lost!')
-                money -= bet
+                print("You lost.")
+                coins -= gamble
+                unsuccessful_attempts +=1
     except ValueError:
-        print('Invalid Bet')
+        print("Invalid Input")
+
+
